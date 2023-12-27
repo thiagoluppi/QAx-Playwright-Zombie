40e93e560c3d7d9cbfaa9608297d59df9ac0efe0
thiagoluppi <57861458+thiagoluppi@users.noreply.github.com>
2023-12-27 15:22:49 +0000

    Finalizando aula Interagindo com GetByLabel

--- tests/pages/MoviesPage.js
+++ tests/pages/MoviesPage.js
@@ -4,10 +4,19 @@ export class MoviesPage {
     constructor(page) {
         this.page = page
         this.addContentButton = this.page.locator("a[href$='register']")
+        this.fieldsPanel = this.page.locator(".fields")
+        this.movieTitleInputField = this.fieldsPanel.locator("input[name=title]")
+        this.overviewField = this.fieldsPanel.locator("textarea[name=overview]")
     }
 
     async addMovie(title, overview, company, release_year) {
         await this.addContentButton.click()
+
+        await this.movieTitleInputField.click()
+        await this.movieTitleInputField.fill(title)
+
+        await this.overviewField.click()
+        await this.overviewField.fill(overview)
 
     }
 }
